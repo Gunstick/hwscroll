d73820612c7bf5db787db856ae2468b1fb7c7424
Georges Kesseler <georgeskesseler@yahoo.com>
2020-01-19 23:04:42 +0100

    added options for various bahaviours

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # hwscroll
 hardware scrolling for Atari STf
 
-This currently only contains the original awk program used to create the ULM 
-hardware scrolling table.
+This is based on the original awk program used to create the ULM 
+hardware scrolling table. To get that version, go back to revision 1 of this repo.
+
+Usage:
+
+By default, calculates a 4 lines table with all known switches/line lengths.
+To use less switches or change number of line use options.
+eclocksync can be used to be able to use all line lengths on the first line
+
+The default is:
+./mkhwscrolltab.sh --lines=4 --lengths=13 --eclocksync on
+
+You can still calculate the original ULM hardware scroller with:
+./mkhwscrolltab.sh --lines 6 --lengths 8 --eclocksync off
 
--- mkhwscrolltab.sh
+++ mkhwscrolltab.sh
@@ -7,27 +7,29 @@
 
 # output is like this:
 # 0$ 0
-# 0$ 0=0000 0000 0000 0000 0000 0000    0   0   0   0   0   0   0
-# 0$ 0=0001 1001 1001 1010 1010 1010   44  70  70  24  24  24  -1
-# 0$ 0=0010 1001 1001 1001 1010 1010   -2  70  70  70  24  24  -1
+# 0$ 0=00000 00000 00000 00000 00000 00000    0   0   0   0   0   0   0
+# 0$ 0=00001 10001 10001 10010 10010 10010   44  70  70  24  24  24  -1
+# 0$ 0=00010 10001 10001 10001 10010 10010   -2  70  70  70  24  24  -1
 # 1$ 2
-# 1$ 2=0000 1001 1001 1001 1010 1010    0  70  70  70  24  24  -1
-# 1$ 2=0001 0001 0010 0010 0010 1100   44  44  -2  -2  -2 -80   0
+# 1$ 2=00000 10001 10001 10001 10010 10010    0  70  70  70  24  24  -1
+# 1$ 2=00001 00001 00010 00010 00010 10100   44  44  -2  -2  -2 -80   0
 # etc...
-# |  |  |                        |      |                       |
-# |  |  |                        |      |                 modulo 256 adjustment
-# |  |  |                        |      1st to 6th line length adjustments
-# |  |  |                        |      from 160 byte standard length
-# |  |  |                        6th line swithes
+# |  |  |                              |      |                       |
+# |  |  |                              |      |                 modulo 256 adjustment
+# |  |  |                              |      1st to 6th line length adjustments
+# |  |  |                              |      from 160 byte standard length
+# |  |  |                              6th line swithes
 # |  |  on/off switches for 1st hwscroll line
 # |  switch number
 # for later sorting
 
 # switches explanation
-# 0001 = we open the right border, line gets 44 bytes longer
-# 0010 = we *close* the right border earlier, line gets 2 bytes shorter
-# 0100 = we close the line in middle of screen, line gets 106 bytes shorter
-# 1000 = we open the left border, adding 26 bytes
+# 000001 = we open the right border, line gets 44 bytes longer
+# 000010 = we *close* the right border earlier, line gets 2 bytes shorter
+# 000100 = we close the line in middle of screen, line gets 106 bytes shorter
+# 010000 = we open the left border, adding 26 bytes
+# 001000 = we start left screen earlier, line gets 2 bytes longer
+# 100000 = we clode the full line, line gets 160 bytes shorter (0 bytes line)
 
 # this gives combinations like 1010 = +26-2 = +24
 # of course things like this are not permitted: 0011 because the border
@@ -41,30 +43,69 @@
 # Because in the first line, only 0xxx is possible, which removes half
 lines=5  # missing: 1, 14, 27, 40, 53, 62, 63, 76, 89, 102, 106, 115
          # 5 lines is usable for vertical hardscoll without overscan
-lines=6  # all possibilities
-if [ "$1" != "" ]
-then
-  lines=$1
-fi
-
+lines=4  # 4 lines full hardscroll with all possibile line lengths
+linelengths=13  # length 13 (0 bytes long) is tricky to use in practice
+eclocksync="on"
+while [ $# -gt 0 ]
+do
+  key="$1"
+  case "$key" in
+    --lines)
+      lines="$2"
+      shift;shift;
+    ;;
+    --lengths)
+      linelengths="$2"
+      shift;shift;
+    ;;
+    --eclocksync)
+      if [ "$2" = "on" ] || [ "$2" = "off" ]
+      then
+        eclocksync="$2"
+      else
+        echo "on or off?"
+        exit
+      fi
+      shift;shift;
+    ;;
+    *)
+    echo "usage: $0 [--lines n] [--lengths n] [--eclocksync {on|off}"
+    echo "default: 4 lines, 13 lengthsi, with eclocksync"
+    echo "13: includes 0 byte line"
+    echo "9-12: includes 2 byte longer line"
+    echo "8: the ULM hardscroll on 6 lines"
+    exit
+    ;;
+  esac
+done
 i=2
 looping=""
 while [ $i -le $lines ]
 do
-  looping="$looping for(scanline[$i]=scanline[$((i-1))];scanline[$i]<=8;scanline[$i]++) "
+  looping="$looping for(scanline[$i]=scanline[$((i-1))];scanline[$i]<=linelengths;scanline[$i]++) "
   i=$((i+1))
 done
-
-awk -v lines=$lines '
+if [ "$eclocksync" = "on" ]
+then
+  eclocksync=$linelengths
+else
+  eclocksync=4 # first line only can have switches after left start
+fi
+awk -v lines=$lines -v linelengths=$linelengths -v eclocksync=$eclocksync '
 BEGIN { 
-        switches[1]="0000";offset[1]=0;s++
-        switches[2]="0001";offset[2]=44;s++
-        switches[3]="0010";offset[3]=-2;s++
-        switches[4]="0100";offset[4]=-106;s++
-        switches[5]="1000";offset[5]=26;s++
-        switches[6]="1001";offset[6]=26+44;s++
-        switches[7]="1010";offset[7]=26-2;s++
-        switches[8]="1100";offset[8]=26-106;
+        switches[1]= "000000";offset[1]=0;s++
+        switches[2]= "000001";offset[2]=44;s++
+        switches[3]= "000010";offset[3]=-2;s++
+        switches[4]= "000100";offset[4]=-106;s++
+        switches[5]= "010000";offset[5]=26;s++
+        switches[6]= "010001";offset[6]=26+44;s++
+        switches[7]= "010010";offset[7]=26-2;s++
+        switches[8]= "010100";offset[8]=26-106;s++
+        switches[9]= "001000";offset[9]=2;s++
+        switches[10]="001100";offset[10]=2-106;s++
+        switches[11]="001010";offset[11]=2-2;s++
+        switches[12]="001001";offset[12]=2+44;s++
+        switches[13]="100000";offset[13]=-160;
 for (i=1;i<=lines;i++)
 {
 scanline[i]=1;
@@ -77,7 +118,7 @@ for (i=0;i<=127;i++)
 }
 
 # now the main loop
-for(scanline[1]=1;scanline[1]<=4;scanline[1]++)
+for(scanline[1]=1;scanline[1]<=eclocksync;scanline[1]++)   # first line can have any switches with e-clock hbl sync
 '"$looping"'
  {
   offs=0
@@ -86,15 +127,15 @@ for(scanline[1]=1;scanline[1]<=4;scanline[1]++)
     offs+=offset[scanline[i]]
   }
  #  printf "offset=%2d ",offs 
-  printf "%3d$%2x=",(offs+5*256)%256/2,(offs+5*256)%256
+  printf "%4d$%3x=",(offs+5*256)%256/2,(offs+5*256)%256
   ok[(offs+5*256)%256]=1
   for (i=1;i<=lines;i++)
   {
-    printf "%s ",switches[scanline[i]]
+    printf " %s ",switches[scanline[i]]
   }
   for (i=1;i<=lines;i++)
   {
-    printf "%4d",offset[scanline[i]]
+    printf " %4d",offset[scanline[i]]
   }
   if (offs >= 0)
   {
